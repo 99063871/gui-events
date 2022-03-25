2be64176fbe4b6b9dd751b17813977ad13cd4d72
99063871 <99063871@mydavinci.com>
2022-03-25 10:39:31 +0100

    clickers gemaakt, je hebt een start knop en knoppen waar je op klikt

--- Simple_FPS_trainer_v1/Simple_FPS_trainer_v1.py
+++ Simple_FPS_trainer_v1/Simple_FPS_trainer_v1.py
@@ -1,20 +1,56 @@
+from distutils.cmd import Command
 import tkinter
 import random
 
 window = tkinter.Tk()
 window.geometry("440x300")
 window.title("Simple FPS trainer")
+
 list = ["Press: W", "Press: A", "Press: S","Press: D", "Press: Space", "Single click", "Double click", "Triple click"]
-def functiona(e):
+
+def functiona(e):    
     global vara
+    global varthing
+    window.unbind(varthing)
+    button.unbind(varthing)
     vara = random.choice(list)
     button.config(text=vara)
-    print("activated")
+    if vara == "Press: W":
+        varthing = '<w>'
+        print("activated")
+    elif vara == "Press: A":
+        varthing = '<a>'
+        print("activated")
+    elif vara == "Press: S":
+        varthing = '<s>'
+        print("activated")
+    elif vara == "Press: D":
+        varthing = '<d>'
+        print("activated")
+    elif vara == "Press: Space":
+        varthing = '<space>'
+        print("activated")
+    elif vara == "Single click":
+        varthing = '<Button-1>'
+        button.bind(varthing, functiona)
+        print("clicked")
+    elif vara == "Double click":
+        varthing = '<Double-Button-1>'
+        button.bind(varthing, functiona)
+        print("dblclicked")
+    elif vara == "Triple click":
+        varthing = '<Triple-Button-1>'
+        button.bind(varthing, functiona)
+        print("trplclicked")
+    else:
+        print("nope")
+        functiona(varthing)
+    window.bind(varthing, functiona)
 
-
-button = tkinter.Button(text="Hi", bg="White", borderwidth=0, width=10, height=2)
+vara = random.choice(list)
+button = tkinter.Button(text="Click here to start", bg="White", borderwidth=0, height=2)
 button.pack()
-functiona(list)
-window.bind('<space>', functiona)
+varthing = '<Button-1>'
+window.bind(varthing , functiona)
 
 window.mainloop()
