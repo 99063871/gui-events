b42fd449931eef0e0d07c7f56084f778d901702f
99063871 <99063871@mydavinci.com>
2022-03-28 09:55:55 +0200

    timer gemaakt, je ziet nu hoe lang je nog hebt

--- Simple_FPS_trainer_v1/Simple_FPS_trainer_v1.py
+++ Simple_FPS_trainer_v1/Simple_FPS_trainer_v1.py
@@ -1,56 +1,81 @@
-from distutils.cmd import Command
 import tkinter
 import random
+import time
+from turtle import pos
 
 window = tkinter.Tk()
 window.geometry("440x300")
+window.configure(bg="gray")
 window.title("Simple FPS trainer")
-
+label = tkinter.Label()
+label.config(bg="gray")
+label.pack()
 list = ["Press: W", "Press: A", "Press: S","Press: D", "Press: Space", "Single click", "Double click", "Triple click"]
+score = 0
+counter = 0
+scoreCount = 0
+timerSecs = 0
 
-def functiona(e):    
+def timer():
+    global timerSecs
+    timerSecs+=1
+    label.config(text="Time remaining: "+str(timerSecs))
+    if timerSecs == 20:
+        button.pack_forget()
+        button.after(1000, button.destroy())
+        print("Je hebt", scoreCount, "punten gehaalt")
+    else:
+        label.after(1000, timer)
+
+def functiona(e):
+    global scoreCount
     global vara
     global varthing
+    global score
+    global events
+    events = "nope"
+    score+=1
+    print(score)
     window.unbind(varthing)
     button.unbind(varthing)
     vara = random.choice(list)
     button.config(text=vara)
+    button.place(x=random.randint(0,390), y=random.randint(0, 250))
+    
     if vara == "Press: W":
         varthing = '<w>'
-        print("activated")
     elif vara == "Press: A":
         varthing = '<a>'
-        print("activated")
     elif vara == "Press: S":
         varthing = '<s>'
-        print("activated")
     elif vara == "Press: D":
         varthing = '<d>'
-        print("activated")
     elif vara == "Press: Space":
         varthing = '<space>'
-        print("activated")
     elif vara == "Single click":
         varthing = '<Button-1>'
-        button.bind(varthing, functiona)
-        print("clicked")
+        events = "click"
     elif vara == "Double click":
         varthing = '<Double-Button-1>'
-        button.bind(varthing, functiona)
-        print("dblclicked")
+        events = "click"
     elif vara == "Triple click":
         varthing = '<Triple-Button-1>'
+        events = "click"
+    
+    if events == "click":
         button.bind(varthing, functiona)
-        print("trplclicked")
+        scoreCount+=1
     else:
-        print("nope")
-        functiona(varthing)
-    window.bind(varthing, functiona)
+        window.bind(varthing, functiona)
+        scoreCount+=1
+    if timerSecs == 0:
+        timer()
 
 vara = random.choice(list)
 button = tkinter.Button(text="Click here to start", bg="White", borderwidth=0, height=2)
-button.pack()
+button.place(x=180, y=120)
 varthing = '<Button-1>'
-window.bind(varthing , functiona)
+button.bind(varthing , functiona)
+score-=1
 
 window.mainloop()
